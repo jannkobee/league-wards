b2397cb0797eb03f23989cf81b1b7307fda1ef3e
Jan Kobe Ignacio <kobe.ignacio@redcoresolutions.com>
2025-07-07 20:22:14 +0800

    feat: netlify version push

--- src/plugins/axios.ts
+++ src/plugins/axios.ts
@@ -1,28 +1,33 @@
 import axios from "axios";
 import { showToast } from "@/composables/useToast";
 
-const apiKey = import.meta.env.VITE_API_KEY;
-
 const axiosRequest = axios.create({
+  baseURL: "/.netlify/functions",
   headers: {
-    "X-Riot-Token": apiKey,
     Accept: "application/json",
   },
 });
 
 axiosRequest.interceptors.response.use(
-  async (res) => {
-    return res;
-  },
+  async (res) => res,
   async (error) => {
-    const { data } = error.response;
+    const { data } = error.response || {};
 
-    showToast({
-      severity: "error",
-      summary: `Error ${data.status.status_code}`,
-      detail: data.status.message,
-      life: 3000,
-    });
+    if (data?.status?.status_code) {
+      showToast({
+        severity: "error",
+        summary: `Error ${data.status.status_code}`,
+        detail: data.status.message,
+        life: 3000,
+      });
+    } else {
+      showToast({
+        severity: "error",
+        summary: "API Error",
+        detail: "An error occurred while processing your request.",
+        life: 3000,
+      });
+    }
 
     return Promise.reject(error);
   },
